f33be951155c18f55b1e6d1cb7b9ae9b3969b8a3
OPaje <jecssilva@gmail.com>
2023-04-13 20:33:56 -0300

    inserir pronto

--- app/src/main/java/com/example/prova1/TelaInserir.kt
+++ app/src/main/java/com/example/prova1/TelaInserir.kt
@@ -1,10 +1,13 @@
 package com.example.prova1
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.prova1.databinding.TelaInserirBinding
 
 class TelaInserir : AppCompatActivity(), AdapterView.OnItemSelectedListener {
@@ -22,6 +25,25 @@ class TelaInserir : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         binding.spOpcoesFazenda.onItemSelectedListener = this
 
+        binding.btnInserir.setOnClickListener {
+            val fazenda = criaFazenda(binding.spOpcoesFazenda.selectedItem.toString())
+
+            val result = Intent()
+            result.putExtra("555", fazenda)
+            setResult(Activity.RESULT_OK, result)
+
+            binding.etNome.text.clear()
+            binding.etCnpj.text.clear()
+            binding.etCar.text.clear()
+            binding.etCaixa.text.clear()
+            binding.etValor5.text.clear()
+
+            Toast.makeText(applicationContext, "Fazenda cadastrada com sucesso", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnHomeTelaInserir.setOnClickListener {
+            finish()
+        }
     }
 
     private fun mudaHint(movel: String){
@@ -29,17 +51,35 @@ class TelaInserir : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             binding.etValor5.hint = "Bacon Estocado"
 
         }
-        if(movel == "Cama"){
+        if(movel == "Vacas leiteiras"){
             binding.etValor5.hint = "Produção Diária"
 
 
         }
-        if(movel == "Estante"){
-            binding.etValor5.hint = "Presença de Silo"
+        if(movel == "Lavoura de soja"){
+            binding.etValor5.setText("Com Silo")
         }
     }
 
-    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+    fun criaFazenda(tipo:String) : Fazenda{
+        var fazenda = Fazenda(binding.etNome.text.toString(), binding.etCnpj.text.toString(), binding.etCar.text.toString(), binding.etCaixa.text.toString().toDouble())
+        if(tipo == "Porcos"){
+            fazenda = Porcos(binding.etValor5.text.toString().toDouble(), binding.etNome.text.toString(), binding.etCnpj.text.toString(), binding.etCar.text.toString(),
+                binding.etCaixa.text.toString().toDouble())
+            return fazenda
+        }else if(tipo == "Vacas leiteiras"){
+            fazenda = VacasLeiteiras(binding.etValor5.text.toString().toDouble(), binding.etNome.text.toString(), binding.etCnpj.text.toString(), binding.etCar.text.toString(),
+                binding.etCaixa.text.toString().toDouble())
+            return fazenda
+        }else if(tipo == "Estante"){
+            fazenda = LavouraDeSoja(true, binding.etNome.text.toString(), binding.etCnpj.text.toString(), binding.etCar.text.toString(),
+                binding.etCaixa.text.toString().toDouble())
+            return fazenda
+        }
+        return fazenda
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
         val valorSelecionado = parent.getItemAtPosition(position).toString()
 
         mudaHint(valorSelecionado)
